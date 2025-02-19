1f4fe37b8d7873eadf45718ac2bde7ad45b7e79f
Stv-devl <lansadstevan@gmail.com>
2025-02-19 16:32:55 +0700

    change header

--- lib/utils/security/securityHeaders.ts
+++ lib/utils/security/securityHeaders.ts
@@ -6,17 +6,16 @@ export const securityHeaders = {
     : process.env.NEXT_PUBLIC_ORIGIN || 'https://syncdata.vercel.app',
   'Access-Control-Allow-Methods': 'POST, PATCH, DELETE, GET, OPTIONS',
   'Access-Control-Allow-Headers':
-    'Content-Type, Authorization, X-Requested-With, Accept, Origin, User-Agent, Referer',
+    'Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Origin, User-Agent, Referer',
   'Content-Type': 'application/json',
-  'Strict-Transport-Security': 'max-age=63072000; includeSubDomains; preload',
+  'Strict-Transport-Security': 'max-age=31536000; includeSubDomains; preload',
   'X-Content-Type-Options': 'nosniff',
-  'Cache-Control': isDev
-    ? 'no-store, no-cache, must-revalidate, proxy-revalidate'
-    : 'public, max-age=600, must-revalidate',
+  'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
   'X-Frame-Options': 'DENY',
   'Content-Security-Policy':
     "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; object-src 'none'; frame-ancestors 'none'; connect-src 'self';",
   'Permissions-Policy':
     'geolocation=(), microphone=(), camera=(), usb=(), magnetometer=()',
+  'Expect-CT': 'max-age=86400, enforce',
   'Referrer-Policy': 'strict-origin-when-cross-origin',
 };

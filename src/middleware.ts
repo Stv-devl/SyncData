2e4ff2c30eb61fdec46b0d6c84909e5538143c62
Stv-devl <lansadstevan@gmail.com>
2025-03-05 14:37:41 +0700

    change midleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,13 +17,5 @@ export default withAuth({
  * @type {Object}
  */
 export const config = {
-  matcher: [
-    '/',
-    '/home',
-    '/profile',
-    '/favorite',
-    '/pricing',
-    '/bin',
-    '/api/:path*',
-  ],
+  matcher: ['/', '/home', '/profile', '/favorite', '/pricing'],
 };

12206135229c8c7e2e37071ac1b14c194c2930a9
Stv-devl <lansadstevan@gmail.com>
2025-03-05 15:26:00 +0700

    put log for vercel

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,15 +4,17 @@ import { withAuth } from 'next-auth/middleware';
  * Middleware for authentication.
  */
 export default withAuth({
+  callbacks: {
+    authorized: ({ token }) => {
+      console.log(' Middleware - Token:', token);
+      return !!token;
+    },
+  },
   pages: {
     signIn: '/login',
   },
 });
 
-/**
- * Configuration du middleware.
- * @type {Object}
- */
 export const config = {
   matcher: ['/home', '/profile', '/favorite', '/pricing'],
 };
--- src/service/getUsers.ts
+++ src/service/getUsers.ts
@@ -10,6 +10,10 @@ import { UserType } from '../types/type';
 const getUsers = async (userId: string): Promise<UserType | null> => {
   const url = `${process.env.NEXT_PUBLIC_API_URL}/users?userId=${userId}`;
 
+  console.log(`fetching user with id ${userId}`);
+
+  console.log('request url', url);
+
   try {
     const response = await fetch(url);
 

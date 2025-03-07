c3540a41eebfc2a04880bb28f27a6b2c5e238778
Stv-devl <lansadstevan@gmail.com>
2025-03-07 15:51:45 +0700

    add fallback

--- src/app/(dashboard)/success/page.tsx
+++ src/app/(dashboard)/success/page.tsx
@@ -1,42 +1,10 @@
-'use client';
+import { Suspense } from 'react';
+import SuccessPage from '../../../components/price/SuccesPage';
 
-import { useSearchParams } from 'next/navigation';
-import { useEffect, useRef } from 'react';
-import { useUserStore } from '@/store/useUserStore';
-
-/**
- * SuccessPage component that validates the subscription payment and updates the user's subscription status.
- * @component
- * @returns {JSX.Element} The rendered SuccessPage component
- */
-const SuccessPage = () => {
-  const searchParams = useSearchParams();
-  const sessionId = searchParams.get('session_id');
-  const { validateSubscription } = useUserStore();
-
-  const hasValidated = useRef(false);
-
-  useEffect(() => {
-    if (!sessionId) {
-      console.log('Missing session ID');
-      return;
-    }
-
-    if (hasValidated.current) return;
-    hasValidated.current = true;
-
-    async function validatePayment() {
-      try {
-        await validateSubscription(sessionId || '');
-      } catch (err) {
-        console.log('Payment validation failed');
-      }
-    }
-
-    validatePayment();
-  }, [sessionId, validateSubscription]);
-
-  return <></>;
-};
-
-export default SuccessPage;
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SuccessPage />
+    </Suspense>
+  );
+}
---
+++ src/components/price/SuccesPage.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import { Suspense, useEffect, useRef } from 'react';
+import { useUserStore } from '@/store/useUserStore';
+
+/**
+ * SuccessPage component that validates the subscription payment and updates the user's subscription status.
+ * @component
+ * @returns {JSX.Element} The rendered SuccessPage component
+ */
+const SuccessPage = () => {
+  const searchParams = useSearchParams();
+  const sessionId = searchParams.get('session_id');
+  const { validateSubscription } = useUserStore();
+
+  const hasValidated = useRef(false);
+
+  useEffect(() => {
+    if (!sessionId) {
+      console.log('Missing session ID');
+      return;
+    }
+
+    if (hasValidated.current) return;
+    hasValidated.current = true;
+
+    async function validatePayment() {
+      try {
+        await validateSubscription(sessionId || '');
+      } catch (err) {
+        console.log('Payment validation failed');
+      }
+    }
+
+    validatePayment();
+  }, [sessionId, validateSubscription]);
+
+  return <></>;
+};
+
+export default SuccessPage;
